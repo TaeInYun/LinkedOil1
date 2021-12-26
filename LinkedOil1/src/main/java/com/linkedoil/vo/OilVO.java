25448855439b0d40ff5eb9d6b3ce3da30f75729e
Jin0207 <86141192+Jin0207@users.noreply.github.com>
2021-12-26 15:26:47 +0900

    OilVO 구현

---
+++ LinkedOil1/src/main/java/com/linkedoil/vo/OilVO.java
@@ -0,0 +1,31 @@
+package com.linkedoil.vo;
+
+public class OilVO {
+	
+	private int oil_code;
+	private String oil_name;
+	
+	public OilVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public OilVO(int oil_code, String oil_name) {
+		super();
+		this.oil_code = oil_code;
+		this.oil_name = oil_name;
+	}
+	public int getOil_code() {
+		return oil_code;
+	}
+	public void setOil_code(int oil_code) {
+		this.oil_code = oil_code;
+	}
+	public String getOil_name() {
+		return oil_name;
+	}
+	public void setOil_name(String oil_name) {
+		this.oil_name = oil_name;
+	}
+	
+	
+}
