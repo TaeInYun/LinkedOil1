20a4614c55d20d0de12bafdf54f605213238d719
vipwhy12 <vipwhy12@naver.com>
2021-12-26 20:16:27 +0900

    ConnectionProvider  공용 데이터베이스로 내용 변경후 완료

--- LinkedOil1/src/main/java/com/linkedoil/db/ConnectionProvider.java
+++ LinkedOil1/src/main/java/com/linkedoil/db/ConnectionProvider.java
@@ -9,16 +9,15 @@ public class ConnectionProvider {
 	public static Connection getConnection() {
 		Connection conn = null;
 		try {
-			String url = "jdbc:oracle:thin:@localhost:1521:XE";
-			String user = "c##test";
-			String password = "test";
+			String url = "jdbc:oracle:thin:@database-2.ckt1x7xytc2q.ap-northeast-2.rds.amazonaws.com:1521:ORACLE";
+			String user = "admin";
+			String password = "admin1234";
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn = DriverManager.getConnection(url, user, password);
 		}catch (Exception e) {
 			System.out.println("예외발생:"+e.getMessage());
 		}
 		return conn;
-		
 	}
 	
 	public static void close(Connection conn, Statement stmt) {
